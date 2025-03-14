72ed8b81d2f0dd62fa2ae3605877ee08ae3fe93e
buffinboback159 <buffinboback159@gmail.com>
2025-03-14 21:48:08 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int age = 25;
+  if (age >= 18) {
+    printf("You can vote!\n");
+  } else {
+    printf("Sorry, you are too young to vote.\n");
+  }
+  return 0;
+}
