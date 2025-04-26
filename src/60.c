1e0ee7d1e4943d2c6b9365c676f48cb38eeca605
buffinboback159 <buffinboback159@gmail.com>
2025-04-26 01:48:08 +0000

    Add 60.c

---
+++ src/60.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int i = 0;
+    
+    while (i < 5) {
+        if (i % 2 == 0) {
+            printf("Even number: %d\n", i);
+        } else {
+            printf("Odd number: %d\n", i);
+        }
+        
+        i++;
+    }
+    
+    return 0;
+}
